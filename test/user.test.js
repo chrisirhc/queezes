bd5ce26e6eb42d01db299e178c2b874e9e56b147
Chris Chua <chris.sirhc@gmail.com>
2010-10-04 16:29:01 +0800

    Open ended questions. Adding the git submodules.
    Testing of adding quizzes (horrible).

--- test/user.test.js
+++ test/user.test.js
@@ -51,5 +51,57 @@ module.exports = {
         assert.equal("null", body, 'Test body shows null');
       });
     });
+  },
+  'test quiz object creation cycle': function() {
+    var quizObj = {
+        name: "Profile",
+        short_name: "p",
+        privacy: 0,
+        creation_time: new Date()
+      };
+    var obj;
+    request({
+      uri: 'http://' + hostname + ':' + portno + "/quiz",
+      method: 'POST',
+      client: httpclient,
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify(quizObj)
+    },
+    function(error, response, body) {
+      assert.equal(201, response.statusCode, 'Test created status');
+      obj = JSON.parse(body);
+      /* Problem with precision of time
+       for (a in quizObj) {
+         assert.equal(quizObj[a], obj[a], 'Test object property ' + a);
+       }
+       */
+      request({
+        uri: 'http://' + hostname + ':' + portno + "/quiz/" + obj.id,
+        method: 'GET',
+        client: httpclient
+      },
+      function(error, response, body) {
+        var testobj = JSON.parse(body);
+        assert.equal(200, response.statusCode, 'Test retrieved status');
+        /** Problem with precision of time **/
+        // assert.deepEqual(testobj, obj, 'Test object');
+      });
+      request({
+        uri: 'http://' + hostname + ':' + portno + "/quiz/" + obj.id,
+        method: 'DELETE',
+        client: httpclient
+      },
+      function(error, response, body) {
+        assert.equal(200, response.statusCode, 'Test deleted status');
+      });
+      request({
+        uri: 'http://' + hostname + ':' + portno + "/quiz/" + obj.id,
+        method: 'GET',
+        client: httpclient
+      },
+      function(error, response, body) {
+        assert.equal("null", body, 'Test body shows null');
+      });
+    });
   }
 };
