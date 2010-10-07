1e8741dc10f5ee348447a61814ad063ad68dc986
Chris Chua <chris.sirhc@gmail.com>
2010-10-07 16:09:19 +0800

    Aesthetics: Adjustments to vertical rhythm.Line height is now 20px.

--- public/js/script.js
+++ public/js/script.js
@@ -2,17 +2,46 @@ var lineheight = 18;
 $(function() {
   var $quiz = $("#quiz"),
     $updownbar = $("#updownbar");
-  if (!$updownbar.length) {
-    $quiz.animate({width: '+=' + 75});
-  }
-  if ($quiz.height() <= $updownbar.height() - lineheight) {
-    $updownbar.fadeOut(function() {
+  /** If it's a quiz page **/
+  if($quiz.length) {
+    if (!$updownbar.length) {
       $quiz.animate({width: '+=' + 75});
+      /*
+    }
+    if ($quiz.height() <= $updownbar.height() - lineheight) {
+      */
+    } else {
+      $updownbar.fadeOut(function() {
+        $quiz.animate({width: '+=' + 75});
+      });
+    }
+      /*
+    } else {
+      $quiz.height(
+        $updownbar.height() - lineheight
+      ).css("overflow", "auto");
+    }
+
+    $allanswerperquestion = $("ol.answers");
+    $("li", $allanswerperquestion).removeClass("chosen")
+      .has(":checked").addClass("chosen");
+    $("li", $allanswerperquestion)
+    $("input[type=text]").focus(function(e) {
+      $(e.target).data("original_value" , $(e.target).val());
     });
-  } else {
-    $quiz.height(
-      $updownbar.height() - lineheight
-    ).css("overflow", "auto");
+    $("input", $allanswerperquestion).change(function(e) {
+      if($(e.target).is("[type=text]")) {
+        if($(e.target).val().length) {
+          $(e.target).closest("li").addClass("chosen");
+        } else {
+          $(e.target).val($(e.target).data("original_value"));
+        }
+      } else {
+        $(e.target).closest("ol").find("li").removeClass("chosen")
+          .has(":checked").addClass("chosen");
+      }
+    });
+  */
   }
   /** Time to add in the scroll to later **/
 });
